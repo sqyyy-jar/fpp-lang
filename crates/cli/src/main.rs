2ee731821722eee137aba11ed99417383d65fab0
sqyyy-jar <62748369+sqyyy-jar@users.noreply.github.com>
2023-09-12 16:19:52 +0200

    feat: make cli somewhat usable

--- crates/cli/src/main.rs
+++ crates/cli/src/main.rs
@@ -1,24 +1,22 @@
+use std::{env::args, fs::read_to_string, io::stdout};
+
 use fpp_compiler::{
-    lir::s7::{self},
+    lir::s7::{self, WriteAwl},
     mir,
     parser::Parser,
 };
 
-const SRC: &[u8] = br#"
-let pos_auf = I0.5;
-let t_stop = I0.1;
-let t_auf = I0.2;
-let tor_schliessen = Q4.1;
-
-tor_schliessen = sr(t_auf, pos_auf & t_stop & tor_schliessen);
-"#;
-
 fn main() {
-    let mut parser = Parser::new(SRC.into());
+    let input = read_to_string(args().nth(1).expect("Input file"))
+        .expect("Readable input file")
+        .as_bytes()
+        .into();
+    let mut parser = Parser::new(input);
     let hir = parser.parse().expect("HIR");
-    println!("{hir:#?}");
+    // println!("{hir:#?}");
     let mir = mir::transformer::transform(hir).expect("MIR");
-    println!("{mir:#?}");
-    let lir = s7::transformer::transform(&mir).expect("LIR");
-    println!("{lir:#?}");
+    // println!("{mir:#?}");
+    let lir = s7::transformer::transform(&mir).expect("S7-LIR");
+    // println!("{lir:#?}");
+    lir.networks[0].write_awl(&mut stdout()).expect("Write AWL");
 }
